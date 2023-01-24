7e6acc26fa2245504cd4b7690ecd5656b072db9e
Bruno Alves <80650110+DioneJA@users.noreply.github.com>
2023-01-24 11:30:24 -0300

    Teste front end

--- front-end/src/components/buttons/BtnAddStudent.vue
+++ front-end/src/components/buttons/BtnAddStudent.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <div class="q-pa-md q-gutter-sm">
-      <q-btn round color="green-6" icon="add" @click="openAddDialog()">
+      <q-btn round color="green-5" icon="add" @click="openAddDialog()">
         <q-tooltip
           :offset="[10, 10]"
           transition-show="scale"
